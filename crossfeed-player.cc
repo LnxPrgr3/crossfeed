b3acc2122dc20c756d9be6a69f1055756f7dc4c4
Jeremy Pepper <jpepper1@gmail.com>
2013-11-27 05:59:45 -0600

    Dispatch playback to dedicated thread
    This will allow us to reclaim main() as the UI thread, which will allow us
    to implement a console UI.

--- crossfeed-player.cc
+++ crossfeed-player.cc
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 #include <CoreFoundation/CoreFoundation.h>
 #include <AudioToolbox/AudioToolbox.h>
+#include <vector>
 #include "cautil.h"
 #include "message_queue.h"
 #include "crossfeed.h"
@@ -14,6 +16,8 @@ struct control_msg {
 static struct message_queue mq;
 static crossfeed_t crossfeed;
 static float scale = 1;
+
+static std::vector<const char *> playlist;
 
 static void event_handler(struct PlayerEvent *evt) {
 	struct control_msg *msg;
@@ -33,6 +37,34 @@ static void event_handler(struct PlayerEvent *evt) {
 	}
 }
 
+static void *audio_threadproc(void *data) {
+	struct Player player;
+	std::vector<const char *>::const_iterator i = playlist.begin();
+	if(CAInitPlayer(&player, &event_handler)) {
+		fprintf(stderr, "Error initializing audio output\n");
+		goto done;
+	}
+	while(i != playlist.end()) {
+		fprintf(stderr, "Playing `%s'...\n", *i);
+		if(CAPlayFile(&player, *i)) {
+			fprintf(stderr, "Error playing `%s'\n", *i);
+			++i;
+			continue;
+		}
+		struct control_msg *msg = (struct control_msg *)message_queue_read(&mq);
+		switch(msg->type) {
+		case control_msg::MSG_FILEDONE:
+			++i;
+			break;
+		}
+		message_queue_message_free(&mq, msg);
+		CAStopPlayback(&player);
+	}
+	CADestroyPlayer(&player);
+done:
+	return data;
+}
+
 int main(int argc, char *argv[]) {
 	int res = EXIT_FAILURE;
 	message_queue_init(&mq, sizeof(struct control_msg), 2);
@@ -41,31 +73,23 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Usage: %s [-g dBFS] /foo/bar\n", argc == 1 ? argv[0] : "crossfeed-player");
 		goto done;
 	}
-	struct Player player;
-	if(CAInitPlayer(&player, &event_handler)) {
-		fprintf(stderr, "Error initializing audio output\n");
-		goto done;
-	}
 	for(int i=1;i<argc;++i) {
 		if(strcmp(argv[i], "-g") == 0) {
 			if(++i > argc)
 				break;
 			float scale_db = atof(argv[i]);
 			scale = pow(10, scale_db/20);
-			fprintf(stderr, "Set gain to %f (%fdBFS)\n", scale, scale_db);
 			continue;
 		}
-		fprintf(stderr, "Playing `%s'...\n", argv[i]);
-		if(CAPlayFile(&player, argv[i])) {
-			fprintf(stderr, "Error playing `%s'\n", argv[i]);
-			continue;
-		}
-		message_queue_message_free(&mq, message_queue_read(&mq));
-		CAStopPlayback(&player);
+		playlist.push_back(argv[i]);
 	}
+	pthread_t audio_thread;
+	if(pthread_create(&audio_thread, NULL, &audio_threadproc, NULL)) {
+		fprintf(stderr, "Error starting audio control thread\n");
+		goto done;
+	}
+	pthread_join(audio_thread, NULL);
 	res = EXIT_SUCCESS;
-close_player:
-	CADestroyPlayer(&player);
 done:
 	message_queue_destroy(&mq);
 	return res;
